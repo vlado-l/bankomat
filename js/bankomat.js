ecda9aee3fe97d1dc864c29fc2fc84c77cb9af57
= <=>
2020-12-13 14:33:48 +0100

    rewritted with vue

--- js/bankomat.js
+++ js/bankomat.js
@@ -1,57 +1,46 @@
-const input = document.querySelector('input')
-const button = document.querySelector('button')
+new Vue({
+    el: "#app",
 
-const nominals = [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-let zasobyKs = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
-let poctyKs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-function render () {
-    $('tbody').html('')
-
-    nominals.forEach((val, idx) => {
-        $('tbody').append(
-            $('<tr>')
-                .append($('<td>', { text: (val / 100).toFixed(2) }))
-                .append($('<td>', { text: poctyKs[idx] }))
-        )
-    })
-}
-
-function vydaj () {
-    let suma = parseFloat(input.value, 10) * 100 || 0
-    let povodnaSuma = suma
-    poctyKs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    let zaloha = [...zasobyKs]
-
-    for (let i = 0; i < nominals.length; i++) {
-        while (suma >= nominals[i]) {
-            if (zasobyKs[i] > 0) {
-                poctyKs[i]++
-                zasobyKs[i]--
-                suma -= nominals[i]
-            } else {
-                break
+    data () {
+        return {
+            suma: '',
+            state: {
+                nominals: [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1],
+                zasoby: [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20],
+                ks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                zaloha: []
             }
         }
-        if (suma === 0) break
-    }
+    },
 
-    if (suma === 0) {
-        render()
-        input.value = ''
-    } else {
-        input.value = ''
-        zasobyKs = zaloha
-        alert(`Nemám dostatok bankoviek\nna vydanie ${(povodnaSuma / 100).toFixed(2)} eur.\nMôžem vydať maximálne ${((povodnaSuma - suma) / 100).toFixed(2)} eur.`)
-    }
-}
+    computed: {
+        parsedSuma: {
+            set (suma) {
+                this.suma = suma
+            },
+            get () {
+                return parseFloat(this.suma, 10) * 100 || 0
+            }
+        }
+    },
 
-render()
-input.focus()
-button.addEventListener('click', vydaj)
-
-document.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter') {
-        vydaj()
+    methods: {
+        vydaj () {
+            let povodnaSuma = this.suma
+            this.ks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            this.zaloha = [...this.ks]
+        
+            for (let i = 0; i < this.state.nominals.length; i++) {
+                while (this.parsedSuma >= this.state.nominals[i]) {
+                    if (this.state.zasoby[i] > 0) {
+                        this.state.ks[i]++
+                        this.state.zasoby[i]--
+                        this.parsedSuma -= this.state.nominals[i]
+                    } else {
+                        break
+                    }
+                }
+            }
+        }
     }
 })
